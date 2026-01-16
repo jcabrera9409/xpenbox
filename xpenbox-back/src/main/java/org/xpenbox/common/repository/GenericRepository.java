b2ed253680264fa9950e84a579831dc8527db488
Jose Cabrera <jcabrera9409@gmail.com>
2026-01-16 00:14:47 -0500

    Add GenericRepository and environment example files for backend and dev configuration

---
+++ xpenbox-back/src/main/java/org/xpenbox/common/repository/GenericRepository.java
@@ -0,0 +1,40 @@
+package org.xpenbox.common.repository;
+
+import java.util.Optional;
+
+import org.jboss.logging.Logger;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+
+/**
+ * Generic Repository class for entities with user association.
+ */
+public class GenericRepository<T> implements PanacheRepository<T> {
+    
+    private final Logger LOG;
+
+    public GenericRepository() {
+        this.LOG = Logger.getLogger(this.getClass());
+    }
+
+    /**
+     * Find an entity by its resource code and user ID.
+     * @param resourceCode the resource code of the entity
+     * @param userId the ID of the user
+     * @return an Optional containing the found entity, or empty if not found
+     */
+    public Optional<T> findByResourceCodeAndUserId(String resourceCode, Long userId) {
+        LOG.infof("Finding entity by resource code: %s and user id: %d", resourceCode, userId);
+        return find("resourceCode = ?1 and user.id = ?2", resourceCode, userId).firstResultOptional();
+    }
+
+    /**
+     * Find all entities associated with a specific user ID.
+     * @param userId the ID of the user
+     * @return a list of entities associated with the user ID
+     */
+    public java.util.List<T> findAllByUserId(Long userId) {
+        LOG.infof("Finding all entities for user id: %d", userId);
+        return list("user.id", userId);
+    }
+}
