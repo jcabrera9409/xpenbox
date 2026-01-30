79bc0ca56312a65e64e6f7f1c2fe23ca6cf83e39
Jose Cabrera <jcabrera9409@gmail.com>
2026-01-29 23:09:25 -0500

    Remove Quarkus Dockerfiles (jvm, legacy-jar, native) and update env.js. Also add .github/ and xpenbox-front/docker/ as new untracked files.

--- xpenbox-front/public/env.js
+++ xpenbox-front/public/env.js
@@ -1,7 +1,6 @@
 (function (window) {
   window.__env = window.__env || {};
-
-  // Estos valores serán reemplazados en tiempo de deploy
+  
   window.__env.production = false;
   window.__env.apiUrl = 'http://localhost:8080/api/rest/v1';
   window.__env.domains = ['localhost:8080'];
