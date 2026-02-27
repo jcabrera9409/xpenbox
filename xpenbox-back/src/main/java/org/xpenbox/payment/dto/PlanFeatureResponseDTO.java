3c3109316dfbe4615b757b024a58aea4175259c3
Jose Cabrera <jcabrera9409@gmail.com>
2026-02-26 20:30:44 -0500

    Fix: update docker-compose env vars, add @RegisterForReflection to PlanFeatureResponseDTO, and reset user state on login

--- xpenbox-back/src/main/java/org/xpenbox/payment/dto/PlanFeatureResponseDTO.java
+++ xpenbox-back/src/main/java/org/xpenbox/payment/dto/PlanFeatureResponseDTO.java
@@ -1,11 +1,14 @@
 package org.xpenbox.payment.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
 /**
  * Data Transfer Object for PlanFeature response. This DTO is used to transfer data from the PlanFeature entity to the client.
  * @param featureCode The code representing the feature.
  * @param limitValue The limit value associated with the feature, if applicable.
  * @param isEnabled Indicates whether the feature is enabled or not.
  */
+@RegisterForReflection
 public record PlanFeatureResponseDTO(
     String featureCode,
     Long limitValue,
