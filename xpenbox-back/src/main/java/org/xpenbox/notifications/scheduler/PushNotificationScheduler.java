4173fee3554c362dab9f9b520eb386f0bce0ef7c
Jose Cabrera <jcabrera9409@gmail.com>
2026-05-13 01:27:45 -0500

    Add scheduled push notification for users without transactions: update scheduler, user repository, and cron config

--- xpenbox-back/src/main/java/org/xpenbox/notifications/scheduler/PushNotificationScheduler.java
+++ xpenbox-back/src/main/java/org/xpenbox/notifications/scheduler/PushNotificationScheduler.java
@@ -1,13 +1,16 @@
 package org.xpenbox.notifications.scheduler;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
+import org.xpenbox.common.DateFunctions;
 import org.xpenbox.notifications.entity.DeviceToken;
 import org.xpenbox.notifications.entity.DeviceToken.Platform;
 import org.xpenbox.notifications.repository.DeviceTokenRepository;
 import org.xpenbox.notifications.service.IPushNotificationService;
+import org.xpenbox.user.entity.User;
+import org.xpenbox.user.repository.UserRepository;
 
 import io.quarkus.scheduler.Scheduled;
 import jakarta.inject.Singleton;
@@ -20,31 +23,58 @@ import jakarta.transaction.Transactional;
 public class PushNotificationScheduler {
     private static final Logger LOG = Logger.getLogger(PushNotificationScheduler.class.getName());
 
-    @ConfigProperty(name = "firebase.push.notification.cron")
-    private String firebasePushNotificationCron;
-
     private final DeviceTokenRepository deviceTokenRepository;
     private final IPushNotificationService pushNotificationService;
+    private final UserRepository userRepository;
 
     public PushNotificationScheduler(
         IPushNotificationService pushNotificationService,
-        DeviceTokenRepository deviceTokenRepository
+        DeviceTokenRepository deviceTokenRepository,
+        UserRepository userRepository
     ) {
         this.pushNotificationService = pushNotificationService;
         this.deviceTokenRepository = deviceTokenRepository;
+        this.userRepository = userRepository;
     }
 
-    @Scheduled(cron = "{firebase.push.notification.cron}")
+    @Scheduled(cron = "{firebase.push.notification.activity.cron}")
     @Transactional
-    void schedulePushNotificationTask() {
-        LOG.info("Running scheduled push notification task");
+    void schedulePushNotificationActivityTask() {
+        LOG.info("Running scheduled push notification activity task");
 
+        List<User> usersWithoutTransactions = findUsersWithoutTransactions();
         List<DeviceToken> deviceTokens = deviceTokenRepository.findAllByStateTrueAndPlatform(Platform.ANDROID);
 
-        for (DeviceToken deviceToken : deviceTokens) {
-            pushNotificationService.sendPushNotification(deviceToken.getToken(), "Prueba Token", "Este es un mensaje de prueba para el token: " + deviceToken.getToken());
+        LOG.infof("Found %d users without transactions", usersWithoutTransactions.size());
+        
+        for (User user : usersWithoutTransactions) {
+            LOG.debugf("User without transactions: %s (ID: %d)", user.getEmail(), user.id);
+            
+            DeviceToken deviceToken = deviceTokens.stream()
+                .filter(dt -> dt.getUser().id.equals(user.id))
+                .findFirst()
+                .orElse(null);
+            
+            if (deviceToken != null) {
+                pushNotificationService.sendPushNotification(
+                    deviceToken.getToken(),
+                    "Un minuto para tus finanzas",
+                    "Registrar tus gastos diariamente te ayuda a tener un mejor control de tu dinero."
+                );
+            } else {
+                LOG.warnf("No active Android device token found for user: %s (ID: %d)", user.getEmail(), user.id);
+            }
         }
 
     }
+
+   private List<User> findUsersWithoutTransactions() {
+        LOG.info("Finding all users without transactions");
+        LocalDateTime now = DateFunctions.currentLocalDateTime();
+        LocalDateTime startDay = now.withHour(0).withMinute(0).withSecond(0).withNano(0);
+        LocalDateTime endDay = now.withHour(23).withMinute(59).withSecond(59).withNano(999999999);
+        return userRepository.findAllUsersWithoutTransactionsAndDates(startDay, endDay);
+    }
+
     
 }
