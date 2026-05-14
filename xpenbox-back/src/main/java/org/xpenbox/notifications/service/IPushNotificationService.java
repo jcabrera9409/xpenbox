b7adb119863b3f201037b8ee85ea8a71da57fc9a
Jose Cabrera <jcabrera9409@gmail.com>
2026-05-14 16:08:12 -0500

    Refactor push notification logic to use batch sending with Firebase, update scheduler and test notification to build Message objects, and update request/collection metadata. Now supports sending multiple notifications efficiently and improves notification payload consistency.

--- xpenbox-back/src/main/java/org/xpenbox/notifications/service/IPushNotificationService.java
+++ xpenbox-back/src/main/java/org/xpenbox/notifications/service/IPushNotificationService.java
@@ -1,6 +1,12 @@
 package org.xpenbox.notifications.service;
 
+import java.util.List;
+
+import com.google.firebase.messaging.Message;
+
 public interface IPushNotificationService {
+
+    void sendPushNotification(List<Message> messages);
 
     void sendPushNotification(String token, String title, String message);
 }
