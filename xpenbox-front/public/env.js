b2656894d77f28de2d26c526875d0e646bfb7d5b
Jose Cabrera <jcabrera9409@gmail.com>
2026-05-13 22:48:27 -0500

    Update API URLs and remove ngrok skip header

--- xpenbox-front/public/env.js
+++ xpenbox-front/public/env.js
@@ -2,10 +2,10 @@
   window.__env = window.__env || {};
   
   window.__env.production = false;
-  window.__env.apiUrl = 'http://localhost:8080/api/rest/v1';
-  window.__env.domains = ['localhost:8080'];
+  window.__env.apiUrl = 'https://xpenbox.firstcode.online/api/rest/v1';
+  window.__env.domains = ['xpenbox.firstcode.online'];
   window.__env.googleAnalyticsId = '';
 
-  window.__env.apiUrlMobile = 'https://xenomorphic-prayerfully-keegan.ngrok-free.dev/api/rest/v1';
-  window.__env.domainsMobile = ['xenomorphic-prayerfully-keegan.ngrok-free.dev'];
+  window.__env.apiUrlMobile = 'https://xpenbox.firstcode.online/api/rest/v1';
+  window.__env.domainsMobile = ['xpenbox.firstcode.online'];
 })(this);
